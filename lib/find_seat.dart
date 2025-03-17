175dca03ee85079e5ea02f6c662115d9bb43e84b
pudhiyamugam <jerinjuleythomas@gmail.com>
2025-03-17 20:14:15 +0530

    updated 2 files find_seat.dart and gec_path.dart

--- lib/find_seat.dart
+++ lib/find_seat.dart
@@ -10,7 +10,55 @@ class SecondPage extends StatelessWidget {
         title: Text('Find your Exam Hall'),
       ),
       body: Center(
-        child: Text(''),
+        child: TextExample(),
+      ),
+    );
+  }
+}
+
+class TextExample extends StatefulWidget {
+  const TextExample({super.key});
+
+  @override
+  State<TextExample> createState() => _TextExampleState();
+}
+
+class _TextExampleState extends State<TextExample> {
+  late TextEditingController _controller;
+
+  @override
+  void initState(){
+    super.initState();
+    _controller=TextEditingController();  }
+
+  @override
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextField(
+          controller: _controller,
+          onSubmitted: (String value){
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text('You have entered:'),
+                content: Text(value),
+                actions: [
+                  ElevatedButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text('OK'),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
--- lib/gec_path.dart
+++ lib/gec_path.dart
@@ -8,8 +8,7 @@ class PathImages extends StatefulWidget {
 }
 
 class _PathImagesState extends State<PathImages> {
-
-  final List<String> images=[
+  final List<String> images = [
     'assets/img_1.png',
     'assets/img_2.png',
     'assets/img_3.png',
@@ -21,50 +20,52 @@ class _PathImagesState extends State<PathImages> {
     'Caption for Image 3',
   ];
 
-  int currentindex=0;
-
+  int currentindex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Path To Exam Hall',
-          style: TextStyle(color: Colors.white),),
+          title: Text(
+            'Path To Exam Hall',
+            style: TextStyle(color: Colors.white),
+          ),
           backgroundColor: Color.fromARGB(255, 130, 6, 207),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-             if(images.isNotEmpty)
-             Image.asset(
-              images[currentindex],
-              height: 300,
-              fit: BoxFit.cover,
-             ),
-             Padding(
+            if (images.isNotEmpty)
+              Image.asset(height: 300, images[currentindex], fit: BoxFit.contain),
+            Padding(
               padding: const EdgeInsets.symmetric(vertical: 20.0),
-             child: Text(texts[currentindex]),),
-             
-              Row(
+              child: Text(texts[currentindex]),
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                ElevatedButton(onPressed:currentindex>0?
-                 () {
-                  setState(() {
-                    currentindex--;
-                  });
-                  }
-                  :null,
-                  child: Text('Previous'),),
-                  ElevatedButton(onPressed: currentindex<images.length-1?(){
-                    setState(() {
-                      currentindex++;
-                    });
-                  }
-                  :null, child: Text('next'))
+                ElevatedButton(
+                  onPressed: currentindex > 0
+                      ? () {
+                          setState(() {
+                            currentindex--;
+                          });
+                        }
+                      : null,
+                  child: Text('Previous'),
+                ),
+                ElevatedButton(
+                    onPressed: currentindex < images.length - 1
+                        ? () {
+                            setState(() {
+                              currentindex++;
+                            });
+                          }
+                        : null,
+                    child: Text('next'))
               ],
-             )
+            )
           ],
         ),
       ),
